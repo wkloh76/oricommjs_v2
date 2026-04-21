2569902e68613a374ff6798f39e2612bf680ed4b
wkloh <wahkiang@gmail.com>
2026-04-21 09:22:37 +0800

    Update entire project license

--- utils/giteapi.js
+++ utils/giteapi.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2026   Loh Wah Kiang
  *
- * openGauss is licensed under Mulan PSL v2.
+ * oricommjs_v2 is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *
